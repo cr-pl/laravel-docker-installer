7199d86abbe4d0921ff2fb52f10d79fd53fc4e9c
Cristian Plugaru <cristian@falcontrading.ro>
2022-11-12 20:02:15 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+printf "\n" >> .env && printf "MYSQL_PASSWORD=" >> .env && openssl rand -base64 21 >> .env && \
+printf "\n"  && printf "MYSQL_ROOT_PASSWORD=" >> .env && openssl rand -base64 21 >> .env  && \
+mkdir -p app && \ 
+docker-compose down && \
+cp "./Dockerfile-php8-no-supervisor" "./app/Dockerfile" && \
+docker-compose up -d --build && \
+rm "./app/Dockerfile" && \ 
+docker-compose exec app composer create-project laravel/laravel . --prefer-dist  && \
+cp "./env-laravel.txt" "./app/.env" && \
+docker-compose exec app php artisan key:generate && \ 
+docker-compose exec app php artisan optimize:clear && \
+docker-compose exec app php artisan migrate:fresh --seed && \
+docker-compose exec app npm install  && \
+docker-compose exec app npm run build && \
+docker-compose exec app php artisan storage:link && \
+chmod -R 0777 app/storage && \
+cp "./Dockerfile-php8-supervisor" "./app/Dockerfile" && \
+docker-compose up -d --build && \
+docker-compose exec app supervisorctl reload
